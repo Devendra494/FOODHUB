f6b183d98d28bf384f79541dbfad427cb2c99a54
@devendrapatil2003 <devendrapatil2003@gmail.com>
2024-01-24 19:09:10 +0530

    added server

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://FOODHUB:devendra143@cluster0.72jyjvg.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB =async() =>{
-   await mongoose.connect(mongoURI, { useNewUrlParser: true}, (err,result)=>{
-    if(err) console.log("___",err)
-    else{
-        console.log("connected");
-    }
+const mongoURI = 'mongodb+srv://FOODHUB:devendra143@cluster0.72jyjvg.mongodb.net/FOODHUB?retryWrites=true&w=majority';
+const mongoDB = async() => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("___", err)
+        else {
+            console.log("connected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray( function( err, data){
+                if(err) console.log(err);
+                else console.log();
+            })
+        }
     });
-    
+
 }
 
 module.exports = mongoDB;
