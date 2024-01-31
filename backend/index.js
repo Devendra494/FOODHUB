61fff6f503e2fc32e3fb4e80ed0c1513fbdafdb8
@devendrapatil2003 <devendrapatil2003@gmail.com>
2024-01-31 19:10:33 +0530

    added project screenshot pdf and orderjs file

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use((req,res,next)=>{
 
 app.use(express.json())
 app.use('/api',require("./Routes/Auth"));
+app.use('/api',require("./Routes/DisplayData"));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
